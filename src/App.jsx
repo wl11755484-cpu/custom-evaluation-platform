2de66a39aa7f1568ca07548f6178936213ccb544
wl11755484-cpu <wl11755484@gmail.com>
2025-08-25 22:47:28 +0800

    Add React source code and main components

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { TaskProvider } from './contexts/TaskContext';
+import TaskListPage from './pages/TaskListPage';
+import AnnotationPage from './pages/AnnotationPage';
+import AnnotationDetailPage from './pages/AnnotationDetailPage';
+import './App.css';
+
+function App() {
+  return (
+    <TaskProvider>
+      <Router>
+        <div className="min-h-screen bg-gray-50">
+          <Routes>
+            <Route path="/" element={<TaskListPage />} />
+            <Route path="/annotation/:taskId" element={<AnnotationPage />} />
+            <Route path="/annotation/:taskId/:dataIndex" element={<AnnotationDetailPage />} />
+          </Routes>
+        </div>
+      </Router>
+    </TaskProvider>
+  );
+}
+
+export default App;
